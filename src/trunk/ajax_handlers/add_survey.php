83075060795487f2c939f072d4a173d925f59958
Simon Tsang <ccskytpk@gmail.com>
2025-05-28 21:23:27 +0800

    feat: create page when create survey

--- src/trunk/ajax_handlers/add_survey.php
+++ src/trunk/ajax_handlers/add_survey.php
@@ -14,16 +14,53 @@ class SurveyJS_AddSurvey extends SurveyJS_AJAX_Handler {
             $table_name = $wpdb->prefix . 'sjs_my_surveys';
 
             $current_user_id = get_current_user_id();
+            $survey_name = sanitize_text_field($_POST['Name']);
+            
             $wpdb->insert( 
                 $table_name, 
                 array( 
-                 'name' => sanitize_text_field($_POST['Name']),
+                 'name' => $survey_name,
                  'user_id' => $current_user_id
                 ) 
             );
+            
+            $survey_id = $wpdb->insert_id;
+            
+            // Create a new page with the survey shortcode
+            $this->create_survey_page($survey_id, $survey_name);
 
-            wp_send_json( array('Id' => $wpdb->insert_id) );
+            wp_send_json( array('Id' => $survey_id) );
         }
+    }
+    
+    /**
+     * Creates a new WordPress page with the survey shortcode embedded
+     * 
+     * @param int $survey_id The ID of the survey
+     * @param string $survey_name The name of the survey
+     * @return int|WP_Error The ID of the created page or WP_Error on failure
+     */
+    private function create_survey_page($survey_id, $survey_name) {
+        // Create a sanitized version of the survey name for the page slug
+        $page_slug = sanitize_title($survey_name);
+        
+        // Create the page content with the shortcode
+        $page_content = sprintf('[Survey id=%d name="%s"]', $survey_id, esc_attr($survey_name));
+        
+        // Set up the page data
+        $page_data = array(
+            'post_title'    => $survey_name,
+            'post_content'  => $page_content,
+            'post_status'   => 'publish',
+            'post_type'     => 'page',
+            'post_name'     => $page_slug,
+            'comment_status' => 'closed'
+        );
+        
+        // Insert the page into the database
+        $page_id = wp_insert_post($page_data);
+        
+        return $page_id;
     }
 }
 
