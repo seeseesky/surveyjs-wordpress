cbd33e08555fa9e12a325b8eb2e88b2506e02407
Simon Tsang <ccskytpk@gmail.com>
2025-05-28 22:04:59 +0800

    fix: delete page also when survey is deleted

--- src/trunk/ajax_handlers/delete_survey.php
+++ src/trunk/ajax_handlers/delete_survey.php
@@ -14,6 +14,9 @@ class SurveyJS_DeleteSurvey extends SurveyJS_AJAX_Handler {
             $table_name = $wpdb->prefix . 'sjs_my_surveys';
             $id = sanitize_key($_POST['Id']);
 
+            // Delete the corresponding page before deleting the survey
+            $this->delete_survey_page($id);
+
             $result = $wpdb->delete( 
                 $table_name, 
                 array( 
@@ -22,6 +25,31 @@ class SurveyJS_DeleteSurvey extends SurveyJS_AJAX_Handler {
             );
 
             wp_send_json( array('IsSuccess' => $result, 'id' => intval($id)) );
+        }
+    }
+    
+    /**
+     * Deletes the WordPress page associated with a survey
+     * 
+     * @param int $survey_id The ID of the survey
+     * @return void
+     */
+    private function delete_survey_page($survey_id) {
+        // Find the page with slug 'survey-{id}'
+        $page_slug = 'survey-' . $survey_id;
+        
+        // Query for the page
+        $pages = get_posts(array(
+            'name' => $page_slug,
+            'post_type' => 'page',
+            'post_status' => 'publish',
+            'numberposts' => 1
+        ));
+        
+        // If the page exists, delete it
+        if (!empty($pages)) {
+            $page_id = $pages[0]->ID;
+            wp_delete_post($page_id, true); // true = force delete (bypass trash)
         }
     }
 }
